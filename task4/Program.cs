e703b922ec9e818aa64b3988b2190143252030e8
Orlikov Artem <orlikov.artemij@gmail.com>
2024-02-23 23:04:24 +0700

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 6;
+int d = 7;
+int c = 9;
+int b = 13;
+int e = 10;
+
+int max = a;
+
+if (d > max);
+{
+    max = d;
+}
+if (c > max);
+{
+    max = c;
+}
+if (b > max);
+{
+    max = b;
+}
+if (e > max);
+{
+    max = e;
+}
+Console.WriteLine(max);
+
